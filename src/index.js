2f422e98fbbf1d75a6a62de17946c92e5f6228d3
Alfred <kalumunabisimwa5@gmail.com>
2022-03-24 23:32:26 +0200

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./style.css";
-import enter from "./assets/enter.svg";
-import refresh from "./assets/rotation.svg";
-import AddRemoveTask from "./add-rmv.js";
+import './style.css';
+import enter from './assets/enter.svg';
+import refresh from './assets/rotation.svg';
+import AddRemoveTask from './add-rmv.js';
 
-const refreshBtn = document.querySelector(".refresh");
-const enterBtn = document.querySelector(".enter");
+const refreshBtn = document.querySelector('.refresh');
+const enterBtn = document.querySelector('.enter');
 
 refreshBtn.src = refresh;
 enterBtn.src = enter;
@@ -14,7 +14,7 @@ const newlib = new AddRemoveTask();
 newlib.submitNewTaskEntry();
 
 window.onload = () => {
-  const store = JSON.parse(localStorage.getItem("Todo-list1")) || [];
+  const store = JSON.parse(localStorage.getItem('Todo-list1')) || [];
   store.forEach((task) => {
     newlib.newTask(task);
   });
